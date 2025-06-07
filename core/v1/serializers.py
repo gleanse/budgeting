ffa6da0c869147befb665f481d6a7a1ba5e1601b
gleanse <auldeyjerusalem2@gmail.com>
2025-06-07 13:50:52 +0800

    created simple API endpoints for budgeting
    currently i just created enpoints for user logging their expenses and for incomes i also create for total money of the user its also simple endpoint andi use django auth module to created endpoint for registration of user acc

--- core/v1/serializers.py
+++ core/v1/serializers.py
@@ -1,5 +1,15 @@
 from rest_framework import serializers
-from ..models import Expense, Income
+from django.contrib.auth.models import User
+from ..models import Expense, Income, Money
+
+
+class User_Serializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['id','username','password'] # this field for now ill add fullname someday i think..
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
 
 class Expense_Serializer(serializers.ModelSerializer):
     class Meta:
@@ -9,4 +19,10 @@ class Expense_Serializer(serializers.ModelSerializer):
 class Income_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Income
+        fields = '__all__'
+
+
+class Money_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Money
         fields = '__all__'
